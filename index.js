ed6b7229eae42b68721c254a87a5bb7ae162117a
Dans7an <kdanstanmark@gmail.com>
2021-12-06 12:20:32 -0500

    second commit

--- index.js
+++ index.js
@@ -21,6 +21,21 @@ app.get("/destinations", (req, res) => {
   res.render("index.ejs", { destinations });
 });
 
+function fetchReq(name, location){
+  try {
+    const API_KEY = "";
+    const UNSPLASH_URL = `https://api.unsplash.com/photos/random?client_id=wQuC3g7JmlN_8yTfq4Hk9247zZRTDzpaZSQyE1b32bE&q=${name} ${location}`;
+
+    const fetchRes = await fetch(UNSPLASH_URL);
+    const data = await fetchRes.json();
+    dest.photo = data.urls.small;
+  } catch (error) {
+    dest.photo =
+      "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80";
+  }
+
+}
+
 app.post("/destinations", async (req, res) => {
   const { name, location, description } = req.body;
 
@@ -31,10 +46,6 @@ app.post("/destinations", async (req, res) => {
   }
 
   const dest = { id: generateUniqueId(), name, location };
-
-  //   if (photo && photo.length !== 0) {
-  //     dest.photo = photo;
-  //   }
 
   try {
     const API_KEY = "";
@@ -94,7 +105,7 @@ app.post("/destinations", async (req, res) => {
 
 app.put("/destinations/:id", (req, res) => {
   const destId = req.params.id;
-  const { name, location, photo, description } = req.body;
+  const { name, location, description } = req.body;
 
   for (let index = 0; index < destinations.length; index++) {
     const element = destinations[index];
@@ -108,10 +119,6 @@ app.put("/destinations/:id", (req, res) => {
 
       if (location && location.length !== 0) {
         element.location = location;
-      }
-
-      if (photo && photo.length !== 0) {
-        element.photo = photo;
       }
 
       if (description && description.length !== 0) {
